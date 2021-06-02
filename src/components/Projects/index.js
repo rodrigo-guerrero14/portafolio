5ff4568736532d056fdf20cc723f17865d97ab34
Rodrigo Guerrero <guerrero.rodrigo14@gmail.com>
2021-06-02 11:35:52 -0400

    the projects section on mobile it was finished

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,45 +1,112 @@
 import '../../assets/styles/projects.css'
+import project_1 from '../../assets/images/project_pabloLee.jpg'
+import project_2 from '../../assets/images/project_sector.jpg'
+import project_3 from '../../assets/images/react_project.png'
 
 function Projects(){
+
   return(
     <section className="projects">
       <div className="container">
         <div className="projects-content">
           <h2 className="title-section">Proyectos</h2>
 
-          <div>
-            <button>Niko</button>
-            <button>Marvol</button>
-            <button>Spotifu</button>
+          <div className="buttons">
+            <a href="#project-1">
+              <button className="btn">Niko</button>
+            </a>
+
+            <a href="#project-2">
+              <button className="btn">Marvol</button>
+            </a>
+
+            <a href="#project-3">
+              <button className="btn">Spotifu</button>
+            </a>
           </div>
 
-          <article>
-            <img src="" alt="niko´s project" />
+          <article id="project-1" className="project">
+            <picture>
+              <img src={project_1} alt="niko´s project" />
+            </picture>
+
             <div className="description">
-              <h4>Niko</h4>
-              <p>Niko es una de las marcas y proveedores deportivo más reconocidas y valiosas en todo el mundo. Por ello este proyecto es dedicado a crear la interfaz de la pagina oficial de Niko, donde tenemos uno de los calzados más...</p>
-              <button>Ver código</button>
-              <button>Ver proyecto</button>
+              <h4>Pablo lee</h4>
+              <p>Landing page con diseño responsivo, se ha desarrollado Mobile first, para facilitar su uso en dispositivos pequeños.</p>
+
+              <h4>Tecnologias usadas</h4>
+              <ul>
+                <li>HTML</li>
+                <li>Vanilla CSS</li>
+                <li>Grid y Flexbox</li>
+              </ul>
+
+              <div className="buttons">
+                <button className="btn">
+                  <a href="https://rodrigo-guerrero14.github.io/pablo-lee/" target="_blank" rel="noreferrer">Ver código</a>
+                </button>
+                <button className="btn">
+                  <a href="https://rodrigo-guerrero14.github.io/pablo-lee/" target="_blank" rel="noreferrer">Ver proyecto</a>
+                </button>
+              </div>
             </div>
           </article>
 
-          <article>
-            <img src="" alt="niko´s project" />
+          <article id="project-2" className="project">
+            <picture>
+              <img src={project_2} alt="sector´s project" />
+            </picture>
+
             <div className="description">
-              <h4>Niko</h4>
-              <p>Niko es una de las marcas y proveedores deportivo más reconocidas y valiosas en todo el mundo. Por ello este proyecto es dedicado a crear la interfaz de la pagina oficial de Niko, donde tenemos uno de los calzados más...</p>
-              <button>Ver código</button>
-              <button>Ver proyecto</button>
+              <h4>Sector</h4>
+              <p>Se ha desarrollado una landing page con el fin de demostrar el conocimiento en tecnologias como HTML, maquetación y CSS, además se ha implementado diseño responsivo, generando diferentes breakpoints para que la aplicacion web se adapte a todo tipo de pantallas.</p>
+
+              <h5>Tecnologias usadas</h5>
+              <ul>
+                <li>HTML</li>
+                <li>Vanilla CSS</li>
+                <li>Grid y Flexbox</li>
+              </ul>
+
+              <div className="buttons">
+                <button className="btn">
+                  <a href="https://rodrigo-guerrero14.github.io/sector/" target="_blank" rel="noreferrer">Ver código</a>
+                </button>
+
+                <button className="btn">
+                  <a href="https://rodrigo-guerrero14.github.io/sector/" target="_blank" rel="noreferrer">Ver proyecto</a>
+                </button>
+              </div>
+
             </div>
           </article>
 
-          <article>
-            <img src="" alt="niko´s project" />
+          <article id="project-3" className="project">
+            <picture>
+              <img src={project_3} alt="niko´s project" />
+            </picture>
+
             <div className="description">
-              <h4>Niko</h4>
-              <p>Niko es una de las marcas y proveedores deportivo más reconocidas y valiosas en todo el mundo. Por ello este proyecto es dedicado a crear la interfaz de la pagina oficial de Niko, donde tenemos uno de los calzados más...</p>
-              <button>Ver código</button>
-              <button>Ver proyecto</button>
+              <h4>Proyecto en React - lista de comentarios</h4>
+              <p>Proyecto desarrollado en react, en donde se ha usado el hook useState para agregar una lista de comentarios, los cuales se renderizan de forma automatica, asi mismo se pueden eliminar estos elementos del DOM.</p>
+
+              <h5>Tecnologias usadas</h5>
+              <ul>
+                <li>React</li>
+                <li>Hooks</li>
+                <li>useState</li>
+                <li>Vanilla CSS</li>
+              </ul>
+
+              <div className="buttons">
+                <button className="btn">
+                  <a href="https://rodrigo-guerrero14.github.io/react-states/" target="_blank" rel="noreferrer">Ver código</a>
+                </button>
+                <button className="btn">
+                  <a href="https://rodrigo-guerrero14.github.io/react-states/" target="_blank" rel="noreferrer">Ver proyecto</a>
+                </button>
+              </div>
+
             </div>
           </article>
         </div>
