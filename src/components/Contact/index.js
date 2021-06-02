f3401dec15e4eecb968d5ddc3f4b3cca3ebf8b17
Rodrigo Guerrero <guerrero.rodrigo14@gmail.com>
2021-06-02 12:52:24 -0400

    contact section was completed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,9 @@
 import '../../assets/styles/contact.css'
 
+const handleSendMessage = (e) => {
+  e.preventDefault()
+}
+
 function Contact(){
   return(
     <section className="contact">
@@ -7,11 +11,11 @@ function Contact(){
         <div className="contact-content">
           <h2 className="title-section">Contact</h2>
           <p> Si está interesado en travajar conmigo en su próximo proyecto, no dude en ponerse en contacto</p>
-          <form action="">
+          <form action="" className="form">
             <input type="text" placeholder="Nombre completo"/>
             <input type="text" placeholder="Correo electrónico"/>
             <textarea name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
-            <button className="btn">Enviar mensaje</button>
+            <button className="btn" onClick={handleSendMessage}>Enviar mensaje</button>
           </form>
         </div>
       </div>
