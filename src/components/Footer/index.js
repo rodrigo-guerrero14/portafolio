c119fa866d8f31ac07036bf45f144cd13168279f
Rodrigo Guerrero <guerrero.rodrigo14@gmail.com>
2021-06-02 17:41:30 -0400

    footer section on mobile devices was finished

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,16 +10,21 @@ function Footer(){
     <footer className="footer">
       <div className="container">
         <div className="footer-content">
-          <h2>Contacto</h2>
-          <ul>
-            <li><a href="www.gmail.com"><img src={email} alt="" />Correo electrónico</a></li>
-            <li><a href="www.instagram.com"><img src={instagram} alt="" />Instagram</a></li>
-            <li><a href="www.twitter.com/"><img src={twitter} alt="" />Twitter</a></li>
-          </ul>
+          <div className="contact-container">
+            <h2 className="title-section">Contacto</h2>
+            <ul>
+              <li><a href="http://www.gmail.com" target="_blank" rel="noreferrer"><img src={email} alt="Email icon" />Correo electrónico</a></li>
+              <li><a href="www.instagram.com"><img src={instagram} alt="Instagram icon" />Instagram</a></li>
+              <li><a href="www.twitter.com/"><img src={twitter} alt="Twitter icon" />Twitter</a></li>
+            </ul>
+          </div>
 
-          <span>Rodrigo Guerrero</span>
-          <p>Hecho con <img src={heart} alt="heart" /> de Rodrigo</p>
-          <p>Copyright 2021 - Todo los derechos reservados</p>
+          <div className="logo-container">
+            <span>Rodrigo Guerrero</span>
+            <p>Hecho con <img src={heart} alt="heart" /> de Rodrigo</p>
+            <p>Copyright 2021 - Todo los derechos reservados</p>
+          </div>
+
         </div>
       </div>
     </footer>
